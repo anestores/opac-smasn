9668855a8040d5e39a949cbc443c4b73d425c5e3
Arnold N. Estores <arnold.estores@technecrest.com>
2019-08-29 07:52:58 +0800

    7th commit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <a class="navbar-brand" href="/">OPAC</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
