0e1848deae2c39675e6fed39996c1041b9a506ed
Arnold N. Estores <arnold.estores@technecrest.com>
2019-08-29 07:33:29 +0800

    3rd commit

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/search-results', 'HomeController@search')->name('search');
 
 
-Route::group(['middleware'=>'checkuser'], function() {
+//Route::group(['middleware'=>'checkuser'], function() {
 
     Route::resource('/subjects', 'SubjectsController');
 
     Route::resource('/books', 'BooksController');
 
     Route::get('/search', 'BooksController@search');
-
-    Route::get('/testpage', function () {
-        return view('testpage');
-    });
-});
+//});
 
